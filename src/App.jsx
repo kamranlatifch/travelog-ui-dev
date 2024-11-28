a37b7f0d062ef0cc55e5ff7e44a48fe0438d9813
Muhammad Kamran Latif <111248908+kamranlatifch@users.noreply.github.com>
2024-11-28 14:19:26 +0500

    feat(shad-cn): add shadcn configurations (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import { Button } from '@components';
 
 function App() {
   return (
     <>
       <div className=''>Travelog</div>
+      <Button>gdgf</Button>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/fleet-ui-dev/',
   plugins: [react()],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
+      '@': path.resolve(__dirname, './src'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components/ui'),
     },
   },
 });
