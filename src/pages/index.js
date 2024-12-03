ff446269722584d2c6441afbf439287472871679
kamranlatifch <kamranlatif98@gmail.com>
2024-12-03 16:24:55 +0500

    feat/services ui add

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 export { Navbar } from './Navbar';
 export { Hero } from './Hero';
 export { Clients } from './Clients';
+export { Services } from './Services';
+export { ServiceCard } from './ServiceCard';
